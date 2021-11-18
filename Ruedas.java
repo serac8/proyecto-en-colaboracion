a9f8d793aace29d1dfe5ea62b46284c9d3a195ba
ikerfc <ikerfc02@gmail.com>
2021-11-18 13:24:46 +0100

    Commit 02 Creados setter y getter de las ruedas

--- Ruedas.java
+++ Ruedas.java
@@ -1,18 +1,27 @@
 public class Ruedas {
 
-	private String marca;
-	private boolean gastadas;
-	
-	public  Ruedas(String marcaBici, boolean ruedasGastadas) {
-	marca = marcaBici;
-	gastadas = ruedasGastadas; 
-	   
-	   
-	   }
-
-
-
-
-
-
+    private String marca;
+    private boolean gastadas;
+    
+    public  Ruedas(String marcaBici, boolean ruedasGastadas) {
+        marca = marcaBici;
+        gastadas = ruedasGastadas; 
+       
+    }
+    
+    public String getMarcaBici(){
+        return marca;
+    }
+    
+    public boolean getRuedasGastadas(){
+        return gastadas;
+    }
+    
+    public void setMarcaNueva(String marcaNueva){
+        marca = marcaNueva;
+    }
+    
+    public void setRuedasGastadas(boolean ruedasNuevas){
+        gastadas = ruedasNuevas;
+    }
 }
