d7fbf955ca3e5bcbe1ef89288ad785bbb2ebf052
serac8 <92148837+serac8@users.noreply.github.com>
2021-11-25 08:59:34 +0100

    Nuevo atributo boolean en clase bici

--- Bici.java
+++ Bici.java
@@ -3,41 +3,47 @@ public class Bici {
     private int kilometros;
     private String marca;
     private Ruedas ruedas;
-    
-    
+    private boolean deMontaña;
+   
+   
     public Bici(int kilometrosRecorridos, String marcaBici, Ruedas ruedasBici) {
        kilometros = kilometrosRecorridos;
        marca = marcaBici;
        ruedas = ruedasBici;
+       deMontaña = true;
     }
    
-    
-    
+   
+   
     public int getkilometros() {
         return kilometros;
     }
-    
+   
     public String getmarca() {
         return marca;
     }
-    
+   
     public Ruedas getruedas() {
         return ruedas;
     }
-    
+   
     public void setkilometrosRecorridos(int kilometrosRecorridos) {
-        kilometros = kilometrosRecorridos; 
+        kilometros = kilometrosRecorridos;
     }
-    
+   
     public void setnuevaMarca(String nuevaMarca) {
         marca = nuevaMarca;
     }
-    
+   
     public void setnuevasRuedas(Ruedas nuevasRuedas) {
         ruedas = nuevasRuedas;
     }
-    
+   
     public void imprimirDetalles(){
-        System.out.println("Kilometros recorridos: "+kilometros+ " - Marca: "+ marca + " - Ruedas marca: "+ ruedas.getMarcaBici()+ " - Ruedas gastadas: "+ ruedas.getRuedasGastadas());
+        System.out.println("Kilometros recorridos: "+kilometros+ " - Marca: "+ marca + " - Ruedas marca: "+ ruedas.getMarcaBici()+ " - Ruedas gastadas: "+ ruedas.getRuedasGastadas() + ". Bici de montaña: " + deMontaña);
+    }
+   
+    public void setDeMontaña(boolean biciDeMontaña){
+        deMontaña = biciDeMontaña;
     }
 }
